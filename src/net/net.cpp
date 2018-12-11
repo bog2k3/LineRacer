a2aaa29f740639633929174c2a9662a2f77fb8d6
Bogdan Ionita <bog2k3@gmail.com>
2018-12-11 15:16:53 +0200

    wip net

--- src/net/net.cpp
+++ src/net/net.cpp
@@ -24,32 +24,39 @@ static void checkStart();	// checks if a worker thread is running and if not it 
 static void checkFinish();	// checks if all connections and listeners are closed and if so, shuts down the worker thread
 static result translateError(const asio::error_code &err);
 
-result startListen(uint16_t port, listener &outLis, newConnectionCallback callback) {
-	std::lock_guard<std::mutex> lk(asyncOpMutex);
-	tcp::acceptor* acceptor = new tcp::acceptor(theIoContext, tcp::endpoint(tcp::v4(), port));
-	listeners.push_back(acceptor);
+result startListenImpl(tcp::acceptor* acceptor, newConnectionCallback callback) {
 	tcp::socket* clientSocket = new tcp::socket(theIoContext);
-	acceptor->async_accept(*clientSocket, [clientSocket, &callback](const asio::error_code& error) {
+	acceptor->async_accept(*clientSocket, [acceptor, clientSocket, callback](const asio::error_code& error) {
+		std::lock_guard<std::mutex> lk(asyncOpMutex);
 		if (!error) {
-			std::lock_guard<std::mutex> lk(asyncOpMutex);
 			connections.push_back(clientSocket);
 			callback(result::ok, connections.size() - 1);
 		} else {
 			delete clientSocket;
 			callback(translateError(error), -1u);
 		}
-		#warning "this will stop after the first connection is accepted, must recall async_accept with a new socket"
+		// recurse to continue listening for new clients:
+		if (acceptor->is_open())	// (only if the operation wasn't canceled meanwhile)
+			startListenImpl(acceptor, callback);
+		else
+			delete acceptor;
 	});
 	checkStart();
 	workAvail.notify_one();
 	return result::ok;
 }
 
+result startListen(uint16_t port, listener &outLis, newConnectionCallback callback) {
+	std::lock_guard<std::mutex> lk(asyncOpMutex);
+	tcp::acceptor* acceptor = new tcp::acceptor(theIoContext, tcp::endpoint(tcp::v4(), port));
+	listeners.push_back(acceptor);
+	return startListenImpl(acceptor, callback);
+}
+
 void stopListen(listener lis) {
 	std::lock_guard<std::mutex> lk(asyncOpMutex);
 	assert(lis < listeners.size() && listeners[lis] != nullptr);
 	listeners[lis]->close();
-	delete listeners[lis];
 	listeners[lis] = nullptr;
 	checkFinish();
 }
@@ -58,7 +65,8 @@ result connect(std::string host, uint16_t port, connection& outCon) {
 	tcp::resolver resolver(theIoContext);
     tcp::resolver::results_type endpoints = resolver.resolve(host, std::to_string(port));
     tcp::socket* newSocket = new tcp::socket(theIoContext);
-    asio::error_code err = asio::connect(*socket, endpoints);
+	asio::error_code err;
+    auto it = asio::connect(*newSocket, endpoints, err);
 	if (!err) {
 		std::lock_guard<std::mutex> lk(asyncOpMutex);
 		connections.push_back(newSocket);
@@ -71,8 +79,26 @@ result connect(std::string host, uint16_t port, connection& outCon) {
 }
 
 void connect_async(std::string host, uint16_t port, newConnectionCallback callback) {
-	std::lock_guard<std::mutex> lk(asyncOpMutex);
-	// ...
+	tcp::resolver resolver(theIoContext);
+    tcp::resolver::query query(host, std::to_string(port));
+	resolver.async_resolve(query, [callback] (const asio::error_code &err, tcp::resolver::iterator endpointIter) {
+		if (err) {
+			callback(translateError(err), -1u);
+		} else {
+			tcp::socket* newSocket = new tcp::socket(theIoContext);
+			asio::async_connect(*newSocket, endpointIter, [newSocket, callback] (const asio::error_code &err, tcp::resolver::iterator endpointIter) {
+				if (!err) {
+					std::lock_guard<std::mutex> lk(asyncOpMutex);
+					connections.push_back(newSocket);
+					callback(result::ok, connections.size()-1);
+				} else {
+					callback(translateError(err), -1u);
+				}
+			});
+			checkStart();
+			workAvail.notify_one();
+		}
+	});
 	checkStart();
 	workAvail.notify_one();
 }
