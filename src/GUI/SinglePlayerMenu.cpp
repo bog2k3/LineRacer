78d1bf45b0975a668081da35e3bf17eb91c33d1a
Bogdan I <bog2k3@gmail.com>
2018-12-14 20:00:44 +0200

    +added round buttons

--- src/GUI/SinglePlayerMenu.cpp
+++ src/GUI/SinglePlayerMenu.cpp
@@ -8,6 +8,12 @@ SinglePlayerMenu::SinglePlayerMenu(glm::vec2 size)
 	: GuiContainerElement(size * margin, size * (1 - 2*margin))
 {
 	setTransparentBackground(true);
+
+	auto pStart = std::make_shared<RoundButton>(glm::vec2{50.f, 50.f}, 30.f, "Start");
+	addElement(pStart);
+	auto pBack = std::make_shared<RoundButton>(glm::vec2{50.f, 150.f}, 30.f, "Back");
+	addElement(pBack);
+
 	/*std::vector<buttonDescriptor> buttons;
 	buttons.push_back({
 		"Start",
